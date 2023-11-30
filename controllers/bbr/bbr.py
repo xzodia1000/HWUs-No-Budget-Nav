24095512f3de5fd3fe347c7582a2cebc33d8c823
NourAdib <naa2000@hw.ac.uk>
2023-11-30 20:52:02 +0400

    Added dynamic shit idk

--- controllers/bbr/bbr.py
+++ controllers/bbr/bbr.py
@@ -44,10 +44,22 @@ class Controller:
         self.center_ir.enable(self.time_step)
         self.right_ir = self.robot.getDevice('gs2')
         self.right_ir.enable(self.time_step)
+
+        self.distance_coef_a = 0.5
+        self.distance_coef_b = -1.3
         
         # Data
         self.inputs = []
         self.inputsPrevious = []
+
+        # Line Following Flags
+        self.follow_line_mode = True
+
+        # Obstacle Avoidance Flags
+        self.avoid_obstacle_mode = False
+        self.distance = 0.0
+        self.distance_tolerance = 0.1
+        self.passed_white_area = False
         
         # Flag
         self.flag_turn = 0
@@ -60,6 +72,7 @@ class Controller:
         self.right_box_counter = 0
         self.right_cylinder_counter = 0
         self.crossed_obstacles = 0
+        self.fork_flag = 0
 
     def turn_right(self):
         # Flags used to stop the robot from auto-correcting itself during the turn
@@ -75,6 +88,33 @@ class Controller:
         self.velocity_left = 1.0
         self.velocity_right = -1.0
 
+    def is_at_distance(self, ir_value):
+        distance = self.distance_coef_a * math.pow(ir_value, self.distance_coef_b)
+
+        if distance < 80:
+            print("Switch to obstacle avoidance mode")
+            self.distance = distance
+            return True
+        else:
+            return False
+
+    def is_on_line(self):
+        if np.min(self.inputs[0:3]) < 0.35:
+            return True
+        else:
+            self.passed_white_area = True
+            return False
+
+    def get_current_distance(self, ir_value):
+        return self.distance_coef_a * math.pow(ir_value, self.distance_coef_b)
+
+    def adjust_steering(self, towards_obstacle):
+        if towards_obstacle:
+            self.velocity_left = 6.8
+            self.velocity_right = -6.8
+        else:
+            self.velocity_left = 0
+            self.velocity_right = 0
     def avoid_left_cylinder(self):
         # First turn right
         if(self.left_cylinder_counter == 0):
@@ -342,6 +382,147 @@ class Controller:
         self.right_motor.setVelocity(self.velocity_right)
         self.is_turning = False
 
+    def avoid_obstacle(self):
+        front_wall = self.get_current_distance(self.inputs[11]) < 80 or self.get_current_distance(self.inputs[17]) < 80
+        left_wall = self.get_current_distance(self.inputs[16]) < 80
+        right_wall = self.get_current_distance(self.inputs[13]) < 80
+        back_wall_right = self.get_current_distance(self.inputs[14]) < 80
+        back_wall_left = self.get_current_distance(self.inputs[15]) < 80
+
+        if (not self.light_detected):
+            if (self.is_on_line() and self.passed_white_area):
+                if (not (back_wall_right)):
+                    self.velocity_left = -self.max_speed
+                    self.velocity_right = self.max_speed
+
+                if (back_wall_right):
+                    self.velocity_left = self.max_speed
+                    self.velocity_right = self.max_speed
+
+                if (back_wall_left):
+                    print("Switch to line following mode")
+                    self.velocity_left = -self.max_speed
+                    self.velocity_right = self.max_speed
+                    self.follow_line_mode = True
+                    self.avoid_obstacle_mode = False
+                    self.passed_white_area = False
+                    self.flag_turn = 1
+                    self.is_turning = False
+            else:
+                if (front_wall):
+                    # Turn left in place
+                    self.velocity_left = -self.max_speed
+                    self.velocity_right = self.max_speed
+                else:
+                    if right_wall:
+                        # Go forward
+                        self.velocity_left = self.max_speed
+                        self.velocity_right = self.max_speed
+                    else:
+                        # Turn right
+                        self.velocity_left = self.max_speed
+                        self.velocity_right = self.max_speed / 4
+        else:
+            if (self.is_on_line() and self.passed_white_area):
+                if (not (back_wall_left)):
+                    self.velocity_left = self.max_speed
+                    self.velocity_right = -self.max_speed
+
+                if (back_wall_left):
+                    self.velocity_left = self.max_speed
+                    self.velocity_right = self.max_speed
+
+                if (back_wall_right):
+                    print("Switch to line following mode")
+                    self.velocity_left = self.max_speed
+                    self.velocity_right = -self.max_speed
+                    self.follow_line_mode = True
+                    self.avoid_obstacle_mode = False
+                    self.passed_white_area = False
+                    self.flag_turn = 1
+                    self.is_turning = False
+            else:
+                if (front_wall):
+                    # Turn left in place
+                    self.velocity_left = self.max_speed
+                    self.velocity_right = -self.max_speed
+                else:
+                    if left_wall:
+                        # Go forward
+                        self.velocity_left = self.max_speed
+                        self.velocity_right = self.max_speed
+                    else:
+                        # Turn left
+                        self.velocity_left = self.max_speed / 4
+                        self.velocity_right = self.max_speed
+
+
+        self.left_motor.setVelocity(self.velocity_left)
+        self.right_motor.setVelocity(self.velocity_right)
+
+    def follow_line(self):
+        # If robot is sensing something
+        if (len(self.inputs) > 0 and len(self.inputsPrevious) > 0):
+
+            ir_value = np.max(self.inputs[3:11])
+            # Check for any possible collision
+            if (ir_value > 0.4):
+
+                if(self.is_at_distance(self.inputs[11])):
+                    self.avoid_obstacle_mode = True
+                    self.follow_line_mode = False
+                    return
+
+                # Check for Light
+                if (self.inputs[3] == 0 and self.inputs[4] == 0 and self.inputs[9] == 0 and self.inputs[10] == 0):
+                    self.light_detected = 1
+                elif (self.inputs[5] == 0):
+                    self.light_detected = 1
+                elif (self.inputs[8] == 0):
+                    self.light_detected = 1
+                elif (self.inputs[6] == 0 and self.inputs[7] == 0):
+                    self.light_detected = 1
+
+                # Check for Fork
+                self.fork_flag = np.isclose(self.inputs[0], 0.76, atol=0.1) and np.isclose(self.inputs[1], 0.76,
+                                                                                           atol=0.1) and np.isclose(
+                    self.inputs[2], 0.76, atol=0.1)
+
+                # If light is on, go left
+                if (self.light_detected and self.fork_flag):
+                    print("Turning Left")
+                    self.turn_left()
+
+                # If light is off, go right
+                if ((not self.light_detected) and self.fork_flag):
+                    print("Turning Right")
+                    self.turn_right()
+            # Automatically adjust to follow line
+            if (self.flag_turn):
+                self.velocity_left = -0.3
+                self.velocity_right = 0.3
+                if (np.min(self.inputs[0:3]) < 0.35):
+                    self.flag_turn = 0
+            else:
+                # Check end of line
+                if ((np.min(self.inputs[0:3]) - np.min(self.inputsPrevious[0:3])) > 0.2):
+                    self.flag_turn = 1
+                else:
+                    # Follow the line
+                    if (not self.is_turning):
+                        if (self.inputs[0] < self.inputs[1] and self.inputs[0] < self.inputs[2]):
+                            self.velocity_left = 0.5
+                            self.velocity_right = 1
+                        elif (self.inputs[1] < self.inputs[0] and self.inputs[1] < self.inputs[2]):
+                            self.velocity_left = 1
+                            self.velocity_right = 1
+                        elif (self.inputs[2] < self.inputs[0] and self.inputs[2] < self.inputs[1]):
+                            self.velocity_left = 1
+                            self.velocity_right = 0.5
+
+        self.left_motor.setVelocity(self.velocity_left)
+        self.right_motor.setVelocity(self.velocity_right)
+        self.is_turning = False
     def run_robot(self):        
         # Main Loop
         count = 0
@@ -368,7 +549,7 @@ class Controller:
             self.inputs.append((center-min_gs)/(max_gs-min_gs)) # 1
             self.inputs.append((right-min_gs)/(max_gs-min_gs)) # 2
             #print("Ground Sensors \n    left {} center {} right {}".format(self.inputs[0],self.inputs[1],self.inputs[2]))
-            
+
            # Read Light Sensors
             for i in range(8):       
                 temp = self.light_sensors[i].getValue()
@@ -380,9 +561,9 @@ class Controller:
                 # Save Data
                 self.inputs.append((temp-min_ls)/(max_ls-min_ls)) # 2 + i
                 #print("Light Sensors - Index: {}  Value: {}".format(i,self.light_sensors[i].getValue()))
-      
+
             # Read Proximity Sensors
-            for i in range(8):       
+            for i in range(8):
                 temp = self.proximity_sensors[i].getValue()
                 # Adjust Values
                 min_ls = 0
@@ -394,22 +575,27 @@ class Controller:
                 #print("Proximity Sensors - Index: {}  Value: {}".format(i,self.proximity_sensors[i].getValue()))
 
             # Smooth filter (Average)
-            smooth = 30
+            smooth = 10
             if(count == smooth):
                 inputs_avg = [sum(x) for x in zip(*inputs_avg)]
                 self.inputs = [x/smooth for x in inputs_avg]
-                # Compute and actuate
-                self.sense_compute_and_actuate()
+
+                if self.follow_line_mode:
+                    # Compute and actuate
+                    self.follow_line()
+
+                if self.avoid_obstacle_mode:
+                    # Compute and actuate
+                    self.avoid_obstacle()
+
+
                 # Reset
                 count = 0
                 inputs_avg = []
                 self.inputsPrevious = self.inputs
             else:
                 inputs_avg.append(self.inputs)
-                count = count + 1 
-
-            # Compute and actuate
-            #self.sense_compute_and_actuate()
+                count = count + 1
                 
             
 if __name__ == "__main__":
